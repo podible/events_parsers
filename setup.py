7e4c1da79157442bed20b0f709484832700c1855
Petr Ryabinin <ryabinin.p.b@gmail.ru>
2023-06-30 20:49:05 +0200

    fix(deps) add tld to setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='events_parsers',
-    version='0.0.37',
+    version='0.0.38',
     author='Podscribe',
     author_email='ivan@podscribe.com',
     description='Parsing row events for Podscribe',
@@ -17,5 +17,5 @@ setuptools.setup(
     },
     license='MIT',
     packages=setuptools.find_packages(),
-    install_requires=['pytz==2022.7.1'],
+    install_requires=['pytz==2022.7.1', 'tld==0.13'],
 )
